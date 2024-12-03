a8ac2f1569c1f704b660bdb56e62f656ecf95a2d
Stefan Wold <ratler@stderr.eu>
2024-12-03 09:58:51 +0100

    day3: part2 added

--- 2024/day3/solution.sh
+++ 2024/day3/solution.sh
@@ -5,6 +5,7 @@ readarray -t memory < input
 main() {
   local sum=0
   local regex="$1"
+  local do=true
 
   for v in "${memory[@]}"; do
     for (( i=0; i<${#v}; i++ )); do
@@ -14,13 +15,20 @@ main() {
         match="${BASH_REMATCH[0]}"
         (( i+=${#match}-1 ))
 
-        val="${match/mul(/}"
-        val="${val/)/}"
-        (( sum+=${val/,/*} ))
+        if [[ $match == "do()" ]]; then
+          do=true
+        elif [[ $match == "don't()" ]]; then
+          do=false
+        elif $do; then
+          val="${match/mul(/}"
+          val="${val/)/}"
+          (( sum+=${val/,/*} ))
+        fi
       fi
     done
   done
   echo "$sum"
 }
 
-main "^mul\([0-9]+,[0-9]+\)"
+echo "Part 1: $(main "^mul\([0-9]+,[0-9]+\)")"
+echo "Part 2: $(main "^mul\([0-9]+,[0-9]+\)|^do\(\)|^don't\(\)")"
